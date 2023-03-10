e771bb7275f91ce0e6ede51b76dbfe8323ae1abc
Ekaterina Dashinova <bulletforkate@gmail.com>
2023-03-11 01:55:13 +0800

    change bonus class

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int price = 10_000;
-        cost = price / 20;
-        System.out.println(cost);
-        return cost;
+        int value;
+        value = cost / 20;
+        System.out.println(value);
+        return value;
     }
 }
